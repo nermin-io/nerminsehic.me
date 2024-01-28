f6c90c00186a2ce5ebe41ed2c25f5e9d1d297013
Nermin <sehicnermin97@gmail.com>
2024-01-28 17:56:21 +1100

    refactor about section

---
+++ app/components/sections/about-section.tsx
@@ -0,0 +1,42 @@
+import { Link } from "@remix-run/react";
+
+export function AboutSection() {
+  return (
+    <section className="space-y-2 py-5 border-b border-background-muted">
+      <h2 className="text-lg font-medium">About</h2>
+      <div className="text-md flex flex-col gap-3 text-foreground-muted">
+        <p>
+          👨‍💻 Hey there, I&apos;m Nermin, an experienced software developer
+          currently immersed in the world of internal tools and automations at{" "}
+          <Link
+            to="https://rendr.delivery"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="special"
+          >
+            Rendr
+          </Link>
+          . My toolkit includes TypeScript, JavaScript, Go, and React, among
+          others. With a keen eye for aesthetics and performance, I thrive on
+          crafting beautiful and efficient user interfaces, though I&apos;m no
+          stranger to backend intricacies.
+        </p>
+        <p>
+          ✨ When I&apos;m not immersed in code, you can often find me at
+          JavaScript meetups in Melbourne, including MelbJS and React Melbourne.
+          These vibrant gatherings are some of the best places to connect with
+          me, share insights, and dive into the latest trends in the JavaScript
+          ecosystem.
+        </p>
+        <p>
+          🚀 Additionally, I enjoy sharing my experiences and insights on
+          software development through technical articles on my blog. I love
+          documenting my journey, from tackling complex problems to embracing
+          new technologies. Join me as I explore the ever-evolving landscape of
+          software development and share the solutions and lessons I discover
+          along the way!
+        </p>
+      </div>
+    </section>
+  );
+}
