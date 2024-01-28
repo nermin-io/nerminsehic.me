8f88bd4f496f8c61d02ad13a6e81023c3afb2242
Nermin <sehicnermin97@gmail.com>
2024-01-28 17:04:45 +1100

    add favicon and manifests

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -15,7 +15,40 @@ import { Footer } from "~/components/footer";
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   { rel: "stylesheet", href: styles },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "mask-icon", href: "/safari-pinned-tab.svg" },
+  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "icon", href: "/favicon.ico" },
 ];
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      name: "msapplication-TileColor",
+      content: "#191919",
+    },
+    {
+      name: "theme-color",
+      content: "#191919",
+    },
+  ];
+};
 
 export default function App() {
   return (
