c0e177f268544d7a9bbb69f34f23e08f14d1f57c
Nermin <sehicnermin97@gmail.com>
2024-01-28 19:37:39 +1100

    add responsive text size

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,12 @@
 export function Header() {
   return (
     <header className="flex flex-col justify-center h-56 gap-2 border-b border-background-muted">
-      <h1 className="font-serif text-5xl font-bold">Nermin Sehic</h1>
-      <p className="text-primary font-serif text-xl">Software Developer</p>
+      <h1 className="font-serif text-5xl lg:text-6xl font-bold">
+        Nermin Sehic
+      </h1>
+      <p className="text-primary font-serif text-xl lg:text-2xl">
+        Software Developer
+      </p>
     </header>
   );
 }
